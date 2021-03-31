72a883f3016a77f073f7fcbcf8647722fa6a15e2
firstof9 <firstof9@gmail.com>
2021-03-31 12:32:29 -0700

    Add missing function

--- grocy-meal-plan-card.js
+++ grocy-meal-plan-card.js
@@ -12,6 +12,19 @@ window.customCards.push({
     preview: false,
 });
 
+const fireEvent = (node, type, detail, options) => {
+    options = options || {};
+    detail = detail === null || detail === undefined ? {} : detail;
+    const event = new Event(type, {
+        bubbles: options.bubbles === undefined ? true : options.bubbles,
+        cancelable: Boolean(options.cancelable),
+        composed: options.composed === undefined ? true : options.composed,
+    });
+    event.detail = detail;
+    node.dispatchEvent(event);
+    return event;
+};
+
 class MealPlanCard extends LitElement {
     static get properties() {
         return {
